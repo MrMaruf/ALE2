c295753f9e6a449dbbe83c99ee328d84d5db7aee
Abdirimov,Maruf M <m.abdirimov@student.fontys.nl>
2020-05-13 05:47:45 +0200

    Parser 1.0 is finished. All tests are passing. Materials updated.

--- ALE2_library/services/Parser.cs
+++ ALE2_library/services/Parser.cs
@@ -10,27 +10,119 @@ namespace ALE2_library.services
 {
     public class Parser
     {
-        public static Transition alphabetLineParser(string line)
+        public static Automaton parseFile(string path)
         {
-            return null;
-        }
-        public static Automaton fileParser(string path)
-        {
-            List<char> alphabet = new List<char>();
+            List<string> alphabet = new List<string>();
             List<Node> states = new List<Node>();
             List<Node> final = new List<Node>();
             List<Transition> transitions = new List<Transition>();
             bool initialDFA = false;
             bool initialFinite = false;
             List<string> words = new List<string>();
-            
+
             List<string> lines = File.ReadAllLines(path).ToList();
             for (int i = 0; i < lines.Count; i++)
             {
 
+                string line = lines[i];
+                
+                if (line.Contains("#")) // ignoring comments
+                    continue;
+
+                if (line.Contains("alphabet"))
+                    alphabet = parseAlphabetLine(line);
+                else if (line.Contains("states"))
+                    states = parseStatesLine(line);
+                else if (line.Contains("final"))
+                    final = parseStatesLine(line);
+                else if (line.Contains("transitions"))
+                {
+                    while (true)
+                    {
+                        line = lines[++i];
+                        if (line.Contains("end"))
+                            break;
+                        transitions.Add(parseTransitionLine(line, states));
+                    }
+                }
+                else if (line.Contains("dfa"))
+                    initialDFA = parseBoolLine(line);
+                else if (line.Contains("finite"))
+                    initialFinite = parseBoolLine(line);
+                else if (line.Contains("words"))
+                {
+                    while (true)
+                    {
+                        line = lines[++i];
+                        if (line.Contains("end"))
+                            break;
+                        words.Add(parseWordsLine(line).Item1);
+                    }
+                }
             }
             Automaton automaton = new Automaton(alphabet, states, final, transitions, initialDFA, initialFinite, words);
             return automaton;
+        }
+
+        public static Transition parseTransitionLine(string line, List<Node> states)
+        {
+            Transition newTransition;
+
+            // removes whitespaces
+            string letter = line.Split(',')[1].Split('-')[0].Trim();
+            Node start = states.Find(x => x.Name == line.Split(',')[0]);
+            Node end = states.Find(x => x.Name == line.Split('>')[1].Trim());
+            newTransition = new Transition(letter, start, end);
+            start.Transitions.Add(newTransition);
+            end.Transitions.Add(newTransition);
+            return newTransition;
+        }
+        
+        public static List<string> parseAlphabetLine(string line)
+        {
+            List<string> letters;
+
+            var concatenatedLetters = line.Split(':')[1].Trim();
+            letters = concatenatedLetters.Select(c => c.ToString()).ToList();
+            
+            return letters;
+        }
+
+        public static List<Node> parseStatesLine(string line)
+        {
+            List<Node> states = new List<Node>();
+
+            var concatenatedStatesNames = line.Split(':')[1].Trim();
+            concatenatedStatesNames.Split(',').ToList().ForEach(x =>
+            {
+                Node newNode = new Node(x.Trim());
+                if (!states.Contains(newNode))
+                    states.Add(newNode);
+            });
+
+            return states;
+
+        }
+        public static bool parseBoolLine(string line)
+        {
+            bool boolValue;
+
+            var stringWithBool = line.Split(':')[1].Trim();
+            boolValue = stringWithBool == "y" ? true : false;
+            
+            return boolValue;
+
+        }
+        public static Tuple<string, bool, bool> parseWordsLine(string line)
+        {
+            Tuple<string, bool, bool> wordValues;
+
+            var splitLine = line.Split(',');
+            string word = splitLine[0].Trim();
+            bool boolValue = splitLine[1].Trim() == "y" ? true : false;
+            wordValues = new Tuple<string, bool, bool>(word, boolValue, false);
+            
+            return wordValues;
         }
     }
 }
--- ALE2_libraryTests/services/ParserTests.cs
+++ ALE2_libraryTests/services/ParserTests.cs
@@ -6,31 +6,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ALE2_library.models;
+using System.Diagnostics;
 
 namespace ALE2_library.services.Tests
 {
     [TestClass()]
     public class ParserTests
     {
-        static Node testNode1 = new Node("TS1", new List<Transition>());
-        static Node testNode2 = new Node("TS2", new List<Transition>());
-        static Transition testTransition1 = new Transition('t', testNode1, testNode2);
-        static string testInputPath = "../../../ALE2_library/materials/input.txt";
+        static Node testNode1 = new Node("TS1");
+        static Node testNode2 = new Node("TS2");
+        static Transition testTransition1 = new Transition("t", testNode1, testNode2);
+        static string testInputPath1 = "../../materials/input.txt";
+        static string testInputPath2 = "../../materials/input2.txt";
+        
         [TestMethod()]
-        public void alphabetLineParser_parsesLine_returnsTransition()
+        public void parseFile_takesPath_returnsAutomatonObject()
+        {
+            string testInput = testInputPath1;
+
+            var file = Parser.parseFile(testInput);
+
+            Assert.IsInstanceOfType(file, typeof(Automaton));
+        }
+        [TestMethod()]
+        public void parseTransitionLine_takesStringLineAndNodeList_returnFilledInTransition()
         {
             Transition expectedResponse = testTransition1;
-            Assert.Fail();
+            string testInput = "TS1,t --> TS2";
+            List<Node> testStates = new List<Node>(new Node[] { testNode1, testNode2 });
+
+            Transition testResult = Parser.parseTransitionLine(testInput, testStates);
+
+            Assert.AreEqual(expectedResponse, testResult,
+                "Test node: " + testResult.Start.Name + "," + testResult.Letter + " --> " + testResult.End.Name);
         }
 
         [TestMethod()]
-        public void fileParserTest_takesPath_returnsAutomaton()
+        public void parseAlphabetLine_takesStringLine_returnsListOfStringsContainingAlphabet()
         {
-            string testPath = testInputPath;
+            List<string> expectedResult = new List<string>(new string[] { "t", "e", "s" });
+            string testInput = "alphabet: tes";
             
-            var file = Parser.fileParser(testPath);
+            var testResult = Parser.parseAlphabetLine(testInput);
 
-            Assert.IsInstanceOfType(file, typeof(Automaton));
+            Assert.AreEqual(expectedResult.Count, testResult.Count);
+            testResult.ForEach(x => Assert.IsTrue(expectedResult.Contains(x)));
+        }
+
+        [TestMethod()]
+        public void parseStatesLine_takesStringLine_returnsListOfNodesContainingStates()
+        {
+            List<Node> expectedResult = new List<Node>(new Node[] { testNode1, testNode2 });
+            string testInput = "states: TS1, TS2";
+
+            var testResult = Parser.parseStatesLine(testInput);
+
+            Assert.AreEqual(expectedResult.Count, testResult.Count);
+            testResult.ForEach(x => Assert.IsTrue(expectedResult.Contains(x)));
+        }
+
+        [TestMethod()]
+        public void parseFinalLine_takesStringLine_returnsListOfNodesContainingStates()
+        {
+            List<Node> expectedResult = new List<Node>(new Node[] { testNode2 });
+            string testInput = "final: TS2";
+
+            var testResult = Parser.parseStatesLine(testInput);
+
+            Assert.AreEqual(expectedResult.Count, testResult.Count);
+            testResult.ForEach(x => Assert.IsTrue(expectedResult.Contains(x)));
+        }
+
+        [TestMethod()]
+        public void parseBoolLine_takesStringLine_returnsBoolean()
+        {
+            bool expectedResult = true;
+            string testInput = "finite: y";
+
+            var testResult = Parser.parseBoolLine(testInput);
+
+            Assert.AreEqual(expectedResult, testResult);
+        }
+
+        [TestMethod()]
+        public void parseWordsLine_takesStringLine_returnsTupleContainingWordAndInitialStatus()
+        {
+            var expectedResult = new Tuple<string, bool, bool>("test", true, false);
+            string testInput = "test, y";
+
+            var testResult = Parser.parseWordsLine(testInput);
+
+            Assert.AreEqual(expectedResult, testResult);
+        }
+
+        [TestMethod()]
+        public void parseFile_takesPath_returnsParsedAutomaton()
+        {
+            var states = new List<Node>(new Node[] { testNode1, testNode2 });
+            var final = new List<Node>(new Node[] { testNode1 });
+            var alphabet = new List<string>(new string[] { "t", "e", "s" });
+            string stack = null;
+            var transitions = new List<Transition>();
+            transitions.Add(new Transition("t", testNode2, testNode1));
+            transitions.Add(new Transition("e", testNode2, testNode1));
+            transitions.Add(new Transition("s", testNode1, testNode2));
+            transitions.Add(new Transition("t", testNode1, testNode2));
+            transitions.Add(new Transition("e", testNode1, testNode2));
+            var words = new List<string>();
+            words.AddRange(new string[]{ "test", "set", "sit"});
+            Automaton expectedResult = new Automaton(alphabet, states, final, transitions, true, false, words);
+            string testInput = testInputPath2;
+            
+            var testResult = Parser.parseFile(testInput);
+
+            Assert.AreEqual(expectedResult, testResult);
         }
     }
 }
